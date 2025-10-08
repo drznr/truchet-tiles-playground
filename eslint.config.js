a87702ec8b3bc696151bc5a90eea3d551c31af4c
drznr <dresnerdaniel@gmail.com>
2025-10-08 19:42:34 +0300

    docs folder for gh-pages

--- eslint.config.js
+++ eslint.config.js
@@ -51,6 +51,6 @@ export default [
   },
 
   {
-    ignores: ['node_modules/**', 'dist/**', 'build/**', 'public/**'],
+    ignores: ['node_modules/**', 'dist/**', 'build/**', 'public/**', 'docs/**'],
   },
 ];
