70a8f383ad8866c3dae2f15aae48620140867cd1
drznr <dresnerdaniel@gmail.com>
2025-10-08 19:20:29 +0300

    colors improvements

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -15,7 +15,7 @@ const DEFAULT_CONFIG = {
   tileSize: 40,
   tileType: TileType.Triangle,
   layoutAlgorithm: TileLayoutAlgorithm.None,
-  colors: { fill: '#333', stroke: '#e2e2e2', background: '#fff' },
+  colors: { fill: '#333', stroke: '#333', background: '#fff' },
 } as const satisfies ChartConfig;
 
 export const chartConfig: ChartConfig = DEFAULT_CONFIG;
