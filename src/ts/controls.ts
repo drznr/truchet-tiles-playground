b4f79d7f8708278afb5861417ff1c583cea7683e
drznr <dresnerdaniel@gmail.com>
2025-10-08 18:14:58 +0300

    control to change layout algo

--- src/ts/controls.ts
+++ src/ts/controls.ts
@@ -1,18 +1,26 @@
 import { setChartConfig } from './config';
 import { render } from './main';
-import type { TileType } from './types';
+import type { TileLayoutAlgorithm, TileType } from './types';
 
 export function initControls() {
   const elTileTypeInput = document.querySelector(
     '.tile-type-selector'
   ) as HTMLInputElement;
-
-  elTileTypeInput.value = 'DIAGONAL';
+  const elLayoutAlgoInput = document.querySelector(
+    '.tile-type-selector'
+  ) as HTMLInputElement;
 
   elTileTypeInput.addEventListener('change', (event) => {
     const { value } = event.target as HTMLInputElement;
 
     setChartConfig('tileType', value as TileType);
+    render();
+  });
+
+  elLayoutAlgoInput.addEventListener('change', (event) => {
+    const { value } = event.target as HTMLInputElement;
+
+    setChartConfig('layoutAlgorithm', value as TileLayoutAlgorithm);
     render();
   });
 }
