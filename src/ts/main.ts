02ea4f611bd7826734a6b02b131414e54a3a1b84
drznr <dresnerdaniel@gmail.com>
2025-10-08 17:02:06 +0300

    use resize observer and debouncing

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,17 +1,27 @@
 import { update } from './update';
+import { debounce } from './utils';
 
 const container = document.querySelector('.chart-container')!;
 
 let rafId: number | null = null;
 
-window.addEventListener('load', render);
-window.addEventListener('resize', render);
+const debouncedRender = debounce(render, 25);
 
-function render() {
+window.addEventListener('load', () => {
+  const { width, height } = container.getBoundingClientRect();
+  render(width, height);
+});
+
+const resizeObserver = new ResizeObserver((entries) => {
+  const [entry] = entries;
+  const { width, height } = entry.contentRect;
+  debouncedRender(width, height);
+});
+
+resizeObserver.observe(container);
+
+function render(width: number, height: number) {
   if (rafId) cancelAnimationFrame(rafId);
 
-  rafId = requestAnimationFrame(() => {
-    const { width, height } = container.getBoundingClientRect();
-    update(width, height);
-  });
+  rafId = requestAnimationFrame(() => update(width, height));
 }
